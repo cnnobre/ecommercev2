586a45e1d37292602491010cd00ee1a47b0d055c
Cleilson Nobre <cleilson_nobre@hotmail.com>
2022-02-07 10:46:46 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -29,7 +31,7 @@ class Page {
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+       if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -53,7 +55,7 @@ class Page {
 
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 }
 
