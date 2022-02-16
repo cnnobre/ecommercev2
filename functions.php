41fb324b8d315e60bbb3d0cca050fb9f22965b05
Cleilson Nobre <cleilson_nobre@hotmail.com>
2022-02-16 15:10:18 -0300

    Meus pedidos e resumo carrinho

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Hcode\Model\Cart;
 use \Hcode\Model\User;
 
 function formatPrice($vlprice)
@@ -26,5 +27,28 @@ function getUserName()
 	return $user->getdesperson();
 
 }
+
+function getCartNrQtd()
+{
+
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return$totals['nrqtd'];
+
+}
+
+function getCartVlSubTotal()
+{
+
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return formatPrice($totals['vlprice']);
+
+}
+
 
 ?>
--- site.php
+++ site.php
@@ -240,7 +240,7 @@ $app->post("/checkout", function(){
 
 	$cart = Cart::getFromSession();
 
-	$totals = $cart->getCalculateTotal();
+	$cart->getCalculateTotal();
 
 	$order = new Order();
 
@@ -563,5 +563,42 @@ $app->get("/boleto/:idorder", function($idorder){
 
 });
 
+$app->get("/profile/orders", function(){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders", [
+		'orders'=>$user->getOrders()		
+	]);
+
+});
+
+$app->get("/profile/orders/:idorder", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = new Cart();
+
+	$cart->get((int)$order->getidcart());
+
+	$cart->getCalculateTotal();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders-detail", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()		
+	]);
+
+});
 
 ?>
